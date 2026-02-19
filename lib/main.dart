6f5df421101b6f77a2ad9b23ea2fa3818769b295
Natalia <a22100220@ceti.mx>
2026-02-18 21:27:12 -0600

    Pantallas y Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; // Este es el archivo que se generó solito
+import 'firebase_options.dart';
 
-void main() async {
-  // 1. Aseguramos que el motor de Flutter esté listo
+import 'app.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // 2. Inicializamos Firebase con la configuración automática
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pack&Go México',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      // Por ahora mostramos una pantalla simple para ver que funciona
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            '¡Pack&Go Conectado a Firebase!', 
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(const PackandGo());
 }
